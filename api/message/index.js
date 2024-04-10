ce3e271500fccbaa25e2547b781f664fdf44fd68
GladysGhost <mukosibuilo@gmail.com>
2024-04-10 15:48:04 +0200

    ll

--- api/message/index.js
+++ api/message/index.js
@@ -1,7 +1,7 @@
 //create cars api using express
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
